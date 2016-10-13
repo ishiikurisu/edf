59c086cae0a3e37114d24357559683446fd75a3b
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-10-13 18:33:34 -0300

    Trying to remove dependency upon github.com/ishiikurisu/OA

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Go support for EDF+
 ===================
 
-This package attempt to provide a Go implementation of the EDF format. It reads EDF+ files into two structures:
+This package provides a [Go](https://golang.org/) implementation of the EDF format. It reads EDF+ files into two structures:
 
 + A map from strings to strings, representing the main header in the EDF file.
 + A slice of slices of int16, each one representing one channel's recording.
--- cwrap.go
+++ cwrap.go
@@ -4,24 +4,67 @@ package edf
 // #include "C/buffer.h"
 // #include "C/csv2ascii.h"
 import "C"
+// import "os"
+import "log"
+import "strings"
+// import "bufio"
 
 // TODO Make this repository not depend upon github.com/ishiikurisu/OA
-func Csv2SingleWoC(inlet string) {
+func Csv2Single(inlet string) {
+	outlet := generateSingleOutput(inlet)
+	// inputFile, _ := os.Open(inlet)
+	// outputFile, _ := os.OpenFile(outlet)
+	// scanner := bufio.NewScanner(fp)
+	// typewriter := bufio.NewWriter(os.OpenFile(outlet, ))
+	// defer fp.Close()
 
+	log.Println(outlet)
+	Csv2SingleWC(inlet)
 }
 
-func Csv2MultipleWoC(inlet string) {
-	
+func Csv2Multiple(inlet string) {
+	C.csv2multiple(C.CString(inlet))
 }
 
 // Converts the generated *.csv file from WriteCSV to a single *.ascii file
 // with every channel recording.
-func Csv2Single(inlet string) {
+func Csv2SingleWC(inlet string) {
     C.csv2single(C.CString(inlet))
 }
 
 // Converts the generated *.csv file from WriteCSV to multiple *.ascii files,
 // as many channels exist in the recording.
-func Csv2Multiple(inlet string) {
+func Csv2MultipleWC(inlet string) {
     C.csv2multiple(C.CString(inlet))
+}
+
+/* ######################
+   # AUXILIAR FUNCTIONS #
+   ###################### */
+
+func generateSingleOutput(inlet string) string {
+	index := len(inlet) - 1
+
+	for inlet[index] != '.' {
+		index--
+	}
+
+	outlet := inlet[0:index] + ".ascii"
+	return outlet
+}
+
+func getLabelsFromHeader(header string)[]string {
+	fields := strings.Split(header, ";")
+
+	// Getting labels field
+
+	// Getting number channels
+
+	// Separating labels' names
+
+	return fields
+}
+
+func getNumberChan(header string) int {
+	return 0
 }
