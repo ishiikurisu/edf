77ff709f5f147628d89338e1f38e2d148110a6cf
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-02-16 10:17:46 -0200

    Enforcing data length

--- edfSave.go
+++ edfSave.go
@@ -3,6 +3,10 @@ package edf
 import "os"
 import "fmt"
 
+/* ##################
+   # MAIN FUNCTIONS #
+   ################## */
+
 // Writes the EDF data to the given provided by the output string.
 // WARNING!! THIS FUNCTION IS EXPERIMENTAL.
 func (edf *Edf) WriteEdf(output string) {
@@ -22,6 +26,7 @@ func (edf *Edf) WriteEdf(output string) {
 
 func writeHeaderHeader(edf *Edf, fp *os.File) {
 	specsList := GetSpecsList()
+	specsLength := GetSpecsLength()
 	limit := len(specsList)
 
 	for index := 0; index < limit; index++ {
@@ -31,9 +36,34 @@ func writeHeaderHeader(edf *Edf, fp *os.File) {
 			break
 		} else {
 			// TODO Enforce length of header data
-			fmt.Fprintf(fp, "%s", edf.Header[spec])
-			fmt.Printf("%s", edf.Header[spec])
+			field := edf.Header[spec]
+			field = enforceSize(field, specsLength[spec])
+			fmt.Fprintf(fp, "%s", field)
+			fmt.Printf("%s: %s\n", spec, field)
 		}
 	}
 	fmt.Println("")
 }
+
+
+/* ######################
+   # AUXILIAR FUNCTIONS #
+   ###################### */
+
+func enforceSize(field string, limit int) string {
+	length := len(field)
+
+	if limit == length {
+
+	} else if length > limit {
+		field = field[0:limit]
+	} else {
+		for i := 0; i < limit - length; i++ {
+			field += " "
+		}
+	}
+
+	return field
+}
+
+/* CODE IS POETRY */
